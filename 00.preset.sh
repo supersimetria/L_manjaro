793db6c43707fc531e2f34552a98643863bdf4e8
pfiliberto599@gmail.com <ylrB"O_I\nJq5aA>
2021-09-08 21:26:44 -0400

    001.002

--- 00.preset.sh
+++ 00.preset.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-#titul---------------+																								#
-#																													#
-#ver.001.001																										#
-#																													#
-#--------------------+																								#
-#																													#
+#titul---------------+
+#
+#ver.001.002
+#
+#--------------------+
+#
 #mirrors------------------------------------------------------------------------------------------------------------+
-sudo pacman-mirrors --fasttrack 9; sudo pacman -Sy --noconfirm														#
+sudo pacman-mirrors --fasttrack 9; sudo pacman -Sy --noconfirm
 #blackarch-repo-----------------------------------------------------------------------------------------------------+
-curl -O https://blackarch.org/strap.sh																				#
-echo 46f035c31d758c077cce8f16cf9381e8def937bb strap.sh | sha1sum -c													#
-chmod +x strap.sh																									#
-sudo sed -i "/nameserver/i nameserver 8.8.8.8" /etc/resolv.conf														#
-gpgconf --kill all																									#
-sudo ./strap.sh -v --noconfirm																						#
-sudo rm /var/lib/pacman/sync/blackarch.db.sig																		#
-sudo rm -r /var/lib/pacman/sync																						#
-rm strap.sh																											#
+curl -O https://blackarch.org/strap.sh
+echo 46f035c31d758c077cce8f16cf9381e8def937bb strap.sh | sha1sum -c
+chmod +x strap.sh
+sudo sed -i "/nameserver/i nameserver 8.8.8.8" /etc/resolv.conf
+gpgconf --kill all
+sudo ./strap.sh -v --noconfirm
+sudo rm /var/lib/pacman/sync/blackarch.db.sig
+sudo rm -r /var/lib/pacman/sync
+rm strap.sh
 #xfconf-query-------------------------------------------------------------------------------------------------------+
-xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/blank-on-ac' -t 'int' -s '0'							#
-xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/brightness-step-count' -t 'int' -s '10'				#
-xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/dpms-enabled' -t 'bool' -s 'false'					#
-xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/dpms-on-ac-off' -t 'int' -s '0'						#
-xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/dpms-on-ac-sleep' -t 'int' -s '0'						#
-xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/general-notification' -t 'bool' -s 'false'			#
-xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/lock-screen-suspend-hibernate' -t 'bool' -s 'false'	#
-xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/logind-handle-lid-switch' -t 'bool' -s 'false'		#
+xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/blank-on-ac' -t 'int' -s '0'
+xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/brightness-step-count' -t 'int' -s '10'
+xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/dpms-enabled' -t 'bool' -s 'false'
+xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/dpms-on-ac-off' -t 'int' -s '0'
+xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/dpms-on-ac-sleep' -t 'int' -s '0'
+xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/general-notification' -t 'bool' -s 'false'
+xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/lock-screen-suspend-hibernate' -t 'bool' -s 'false'
+xfconf-query -c xfce4-power-manager -np '/xfce4-power-manager/logind-handle-lid-switch' -t 'bool' -s 'false'
 #firs-update--------------------------------------------------------------------------------------------------------+
-sudo pacman -Syu --noconfirm																						#
+sudo pacman -Syu --noconfirm
 #link-file----------------------------------------------------------------------------------------------------------+
-#link to git.io																										#
+#link https://git.io/JuBUl
--- 03.useradd.sh
+++ 03.useradd.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
-#titul---------------+                                                                                              #
-#                                                                                                                   #
-#ver.001.001                                                                                                        #
-#                                                                                                                   #
-#--------------------+                                                                                              #
-#                                                                                                                   #
+#titul---------------+
+#
+#ver.001.002
+#
+#--------------------+
+#
 #user-group-create--------------------------------------------------------------------------------------------------+
-sudo groupadd -g 109 vboxsf                                                                                         #
-sudo groupadd -g 6941 user_03                                                                                       #
-sudo useradd -m -u 1214 -g 6941 -G 3,11,90,98,108,109,991,998 -s /bin/zsh user_00 #alan_smithee                     #
-sudo useradd -m -u 2897 -g 6941 -G 3,11,90,98,108,109,991,998 -s /bin/zsh user_01 #brigid_tenenbaum                 #
-sudo useradd -m -u 3180 -g 6941 -G 3,11,90,98,108,109,991,998 -s /bin/zsh user_02 #martin_margiela                  #
-sudo useradd -m -u 9165 -g 6941 -G 3,11,90,98,108,109,991,998 -s /bin/zsh user_03 #super_simetria                   #
+sudo groupadd -g 109 vboxsf
+sudo groupadd -g 6941 user_03
+sudo useradd -m -u 1214 -g 6941 -G 3,11,90,98,108,109,991,998 -s /bin/zsh user_00 #alan_smithee
+sudo useradd -m -u 2897 -g 6941 -G 3,11,90,98,108,109,991,998 -s /bin/zsh user_01 #brigid_tenenbaum
+sudo useradd -m -u 3180 -g 6941 -G 3,11,90,98,108,109,991,998 -s /bin/zsh user_02 #martin_margiela
+sudo useradd -m -u 9165 -g 6941 -G 3,11,90,98,108,109,991,998 -s /bin/zsh user_03 #super_simetria
 #password-----------------------------------------------------------------------------------------------------------+
-(                                                                                                                   #
-  echo 1234;                                                                                                        #
-  echo 1234;                                                                                                        #
-) | sudo passwd user_00                                                                                             #
-(                                                                                                                   #
-  echo 1234;                                                                                                        #
-  echo 1234;                                                                                                        #
-) | sudo passwd user_01                                                                                             #
-(                                                                                                                   #
-  echo 1234;                                                                                                        #
-  echo 1234;                                                                                                        #
-) | sudo passwd user_02                                                                                             #
-(                                                                                                                   #
-  echo 1234;                                                                                                        #
-  echo 1234;                                                                                                        #
-) | sudo passwd user_03                                                                                             #
+(
+ echo 1234;
+ echo 1234;
+) | sudo passwd user_00
+(
+ echo 1234;
+ echo 1234;
+) | sudo passwd user_01
+(
+ echo 1234;
+ echo 1234;
+) | sudo passwd user_02
+(
+ echo 1234;
+ echo 1234;
+) | sudo passwd user_03
 #link-file----------------------------------------------------------------------------------------------------------+
-#link to git.io                                                                                                     #
+#link https://git.io/JuBUd
