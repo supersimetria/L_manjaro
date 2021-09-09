9439bf762f2f1650d4467e91de4ae4521c9c5ff5
pfiliberto599@gmail.com <ylrB"O_I\nJq5aA>
2021-09-08 21:02:37 -0400

    yes

--- 01.packages.sh
+++ 01.packages.sh
@@ -1,48 +1,49 @@
 #!/bin/bash
-#titul---------------+																								#
-#																													#
-#ver.001.001																										#
-#																													#
-#--------------------+																								#
-#																													#
+#titul---------------+																								                                              #
+#																													                                                          #
+#ver.001.001																										                                                    #
+#																													                                                          #
+#--------------------+																								                                              #
+#																	                                                          												#
 #download-packeges--------------------------------------------------------------------------------------------------+
-sudo pacman -Sw $(cat ~/Desktop/tmp_Lunar/mod/01.1.pl-0) --noconfirm												#
+sudo pacman -Sw $(cat ~/Desktop/tmp_Lunar/mod/01.1.pl-0) --noconfirm												                        #
 #install-packeges---------------------------------------------------------------------------------------------------+
-sudo pacman -S $(cat ~/Desktop/tmp_Lunar/mod/01.1.pl-0) --noconfirm													#
+sudo pacman -S $(cat ~/Desktop/tmp_Lunar/mod/01.1.pl-0) --noconfirm													                        #
 #AUR-packeges-------------------------------------------------------------------------------------------------------+
-yay -S anydesk-bin archtorify-git aur/archivebox dirsyncpro drawio-desktop-bin dupeguru figma-linux --noconfirm		#
-yay -S flat-remix flat-remix-gtk fslint iwscanner joplin megasync-bin nerd-fonts-fira-code --noconfirm				#
-yay -S onioncircuits openoffice-bin spotify themix-full-git --noconfirm												#
+yay -S anydesk-bin archtorify-git aur/archivebox dirsyncpro drawio-desktop-bin dupeguru figma-linux --noconfirm		  #
+yay -S flat-remix flat-remix-gtk fslint iwscanner joplin megasync-bin nerd-fonts-fira-code --noconfirm				      #
+yay -S onioncircuits openoffice-bin spotify themix-full-git --noconfirm												                      #
 #link-AUR-packeges--------------------------------------------------------------------------------------------------+
-#-----	anydesk-bin					#-----link 	https://aur.archlinux.org/packages/anydesk-bin/						#
-#-----	archtorify-git				#-----link 	https://aur.archlinux.org/packages/archtorify-git/					#
-#-----	aur/archivebox				#-----link	https://aur.archlinux.org/packages/archivebox/						#
-#-----	dirsyncpro					#-----link	https://aur.archlinux.org/packages/dirsyncpro/						#
-#-----	drawio-desktop-bin			#-----link	https://aur.archlinux.org/packages/drawio-desktop-bin/				#
-#-----	dupeguru 					#-----link	https://aur.archlinux.org/packages/dupeguru/						#
-#-----	figma-linux					#-----link	https://aur.archlinux.org/packages/figma-linux/						#
-#-----	flat-remix 					#-----link	https://aur.archlinux.org/packages/flat-remix/						#
-#-----	flat-remix-gtk 				#-----link	https://aur.archlinux.org/packages/flat-remix-gtk/					#
-#-----	fslint 						#-----link	https://aur.archlinux.org/packages/fslint/							#
-#-----	iwscanner					#-----link	https://aur.archlinux.org/packages/iwscanner/						#
-#-----	joplin 						#-----link	https://aur.archlinux.org/packages/joplin/							#
-#-----	megasync-bin				#-----link	https://aur.archlinux.org/packages/megasync-bin/					#
-#-----	nerd-fonts-fira-code 		#-----link	https://aur.archlinux.org/packages/nerd-fonts-fira-code/			#
-#-----	onioncircuits 				#-----link	https://aur.archlinux.org/packages/onioncircuits/					#															
-#-----	openoffice-bin 				#-----link	https://aur.archlinux.org/packages/openoffice-bin/					#
-#-----	spotify 					#-----link	https://aur.archlinux.org/packages/spotify/							#
-#-----	themix-full-git				#-----link	https://aur.archlinux.org/packages/themix-full-git/					#
+#-----  anydesk-bin           #-----link  https://aur.archlinux.org/packages/anydesk-bin/                           #
+#-----  archtorify-git        #-----link  https://aur.archlinux.org/packages/archtorify-git/					              #
+#-----  aur/archivebox        #-----link  https://aur.archlinux.org/packages/archivebox/						                #
+#-----  dirsyncpro            #-----link  https://aur.archlinux.org/packages/dirsyncpro/                            #
+#-----  drawio-desktop-bin    #-----link  https://aur.archlinux.org/packages/drawio-desktop-bin/                    #
+#-----  dupeguru              #-----link  https://aur.archlinux.org/packages/dupeguru/                              #
+#-----  figma-linux           #-----link  https://aur.archlinux.org/packages/figma-linux/                           #
+#-----  flat-remix            #-----link  https://aur.archlinux.org/packages/flat-remix/						                #
+#-----  flat-remix-gtk        #-----link  https://aur.archlinux.org/packages/flat-remix-gtk/					              #
+#-----  fslint                #-----link  https://aur.archlinux.org/packages/fslint/                                #
+#-----  iwscanner             #-----link  https://aur.archlinux.org/packages/iwscanner/                             #
+#-----  joplin                #-----link  https://aur.archlinux.org/packages/joplin/                                #
+#-----  megasync-bin          #-----link  https://aur.archlinux.org/packages/megasync-bin/                          #
+#-----  nerd-fonts-fira-code  #-----link  https://aur.archlinux.org/packages/nerd-fonts-fira-code/                  #
+#-----  onioncircuits         #-----link  https://aur.archlinux.org/packages/onioncircuits/					                #
+#-----  openoffice-bin        #-----link  https://aur.archlinux.org/packages/openoffice-bin/					              #
+#-----  spotify               #-----link  https://aur.archlinux.org/packages/spotify/                               #
+#-----  themix-full-git       #-----link  https://aur.archlinux.org/packages/themix-full-git/					              #
+#-----  trilium-bin           #-----link  https://aur.archlinux.org/packages/trilium-bin/                           #
 #pip----------------------------------------------------------------------------------------------------------------+
-pip install b0mb3r																									#
+pip install b0mb3r																									                                                #
 #systenctl----------------------------------------------------------------------------------------------------------+
-sudo systemctl stop NetworkManager																					#
-sudo systemctl disable NetworkManager																				#
-sudo systemctl mask NetworkManager																					#
-sudo systemctl unmask NetworkManager																				#
-sudo systemctl start NetworkManager																					#
-sudo systemctl enable sshd.service																					#
-sudo systemctl start sshd.service																					#
-sudo systemctl enable tor.service																					#
-sudo systemctl start tor.service																					#
+sudo systemctl stop NetworkManager																					                                        #
+sudo systemctl disable NetworkManager																				                                        #
+sudo systemctl mask NetworkManager																					                                        #
+sudo systemctl unmask NetworkManager																				                                        #
+sudo systemctl start NetworkManager																					                                        #
+sudo systemctl enable sshd.service																					                                        #
+sudo systemctl start sshd.service																					                                          #
+sudo systemctl enable tor.service																					                                          #
+sudo systemctl start tor.service																					                                          #
 #link-file----------------------------------------------------------------------------------------------------------+
-#link to git.io																										#
+#link to git.io																										                                                  #
