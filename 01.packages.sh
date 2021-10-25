90f7dccab37f885512771c5c42443dd4490431f2
pfiliberto599@gmail.com <ylrB"O_I\nJq5aA>
2021-10-25 07:18:42 -0400

    001.008

--- 01.packages.sh
+++ 01.packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #titul---------------+
 #
-#ver.001.007
+#ver.001.008
 #
 #--------------------+
 #
@@ -10,12 +10,12 @@ sudo pacman -Sw $(curl https://raw.githubusercontent.com/supersimetria/L_manjaro
 #install-packeges---------------------------------------------------------------------------------------------------+
 sudo pacman -S $(curl https://raw.githubusercontent.com/supersimetria/L_manjaro/main/mod/pl-0) --noconfirm
 #AUR-packeges-------------------------------------------------------------------------------------------------------+
-yay -S anydesk-bin archtorify-git dirsyncpro \
+yay -S anydesk-bin archtorify-git freefilesync-bin \
   drawio-desktop-bin dupeguru figma-linux \
   flat-remix flat-remix-gtk fslint \
   iwscanner joplin megasync-bin \
   nerd-fonts-fira-code onioncircuits spotify \
-  themix-full-git trilium-bin \
+  themix-full-git trilium-bin quickhash-gui-bin \
 --noconfirm
 #link-file----------------------------------------------------------------------------------------------------------+
 #link https://git.io/JuBTc
