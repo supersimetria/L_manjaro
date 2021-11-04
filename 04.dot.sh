513130546560d1e3783df7cc7fa33ef9a2a96958
pfiliberto599@gmail.com <ylrB"O_I\nJq5aA>
2021-11-04 09:49:23 -0400

    001.004

--- 04.dot.sh
+++ 04.dot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #titul---------------+
 #
-#ver.001.003
+#ver.001.004
 #
 #--------------------+
 #
@@ -12,6 +12,6 @@
 sed -i '11 s/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
 sed -i '73 s/plugins=(git)/plugins=(npm git npm osx rake sudo dotenv vagrant bundler composer web-search)/g' ~/.zshrc
 git clone https://github.com/powerline/fonts.git
-cd fonts; ./install.sh; cd ..; rm -rf fonts
+sh fonts/install.sh; rm -rf fonts/
 #link-file----------------------------------------------------------------------------------------------------------+
 #link https://git.io/JuBUp
