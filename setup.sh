8f1c6d0f03f34a24c7c863d4da66fb35a03be2be
pfiliberto599@gmail.com <ylrB"O_I\nJq5aA>
2021-12-11 17:28:40 -0500

    001.003

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #titul---------------+
 #
-#ver.001.002
+#ver.001.003
 #
 #--------------------+
 #
 #-------------------------------------------------------------------------------------------------------------------+
 wget -O - https://git.io/JuBUl | bash
 wget -O - https://git.io/JuBTc | bash
-sh ~/
-sh ~/
-sh ~/
+wget -O - https://git.io/JuBUw | bash
+wget -O - https://git.io/JuBUd | bash
+wget -O - https://git.io/JDZXK | bash
 #link-file----------------------------------------------------------------------------------------------------------+
 #link https://git.io/JuBTt
