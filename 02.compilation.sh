75cc56c425bae7273e8fb918c3803e28a43aabe0
pfiliberto599@gmail.com <ylrB"O_I\nJq5aA>
2022-01-02 14:10:27 -0500

    001.001

--- 02.compilation.sh
+++ 02.compilation.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #titul---------------+
 #
-#ver.001.009
+#ver.001.001
 #
 #--------------------+
 #
-#folders------------------------------------------------------------------------------------------------------------+
+# Folders-----------------------------------------------------------------------
 cd /opt/ && sudo mkdir -pm 777 dpkg && cd dpkg; sudo mkdir -pm 777 routerscaner
-#librefox-----------------------------------------------------------------------------------------------------------+
+printf "# ------------------------------------------------------------------------------\n"
+printf "# Librefox\n"
+printf "# ------------------------------------------------------------------------------\n"
 killall -i -v -w firefox
 sudo mkdir /var/tmp/librefox_temporarily; cd /var/tmp/librefox_temporarily
 sudo wget https://github.com/intika/Librefox/releases/download/Librefox-v2.1-v64.0.0/Librefox-2.1-Firefox-Linux-64.0.0.zip -N
@@ -15,7 +17,9 @@ sudo unzip -n Librefox*
 sudo rm -r /usr/lib/firefox/defaults/ /usr/lib/firefox/distribution/
 sudo cp -r defaults/ distribution/ mozilla.cfg Readme.Installation.Instructions.txt /usr/lib/firefox/
 sudo rm -r /var/tmp/librefox_temporarily
-#routerscaner-------------------------------------------------------------------------------------------------------+
+printf "# ------------------------------------------------------------------------------\n"
+printf "# Routerscaner\n"
+printf "# ------------------------------------------------------------------------------\n"
 cd /opt/dpkg/routerscaner
 wget http://msk1.stascorp.com/routerscan/prerelease.7z -O routerscaner.7z
 7z x *.7z; rm *.7z; cd
@@ -23,6 +27,7 @@ wget http://msk1.stascorp.com/routerscan/prerelease.7z -O routerscaner.7z
 #                                                                                                                   +
 #                                                      github                                                       +
 #                                                                                                                   +
+#*******************************************************************************************************************+
 #graphite-theme-----------------------------------------------------------------------------------------------------*
 #git clone https://github.com/vinceliuice/Graphite-gtk-theme.git
 #sh Graphite-gtk-theme/install.sh --tweaks rimless
@@ -31,5 +36,5 @@ wget http://msk1.stascorp.com/routerscan/prerelease.7z -O routerscaner.7z
 #git clone https://github.com/vinceliuice/Graphite-cursors.git
 #sh Graphite-cursors/install.sh
 #sudo rm -r Graphite-cursors
-#link-file----------------------------------------------------------------------------------------------------------+
-#link https://git.io/JuBUw
+# link_file---------------------------------------------------------------------
+# git.io
