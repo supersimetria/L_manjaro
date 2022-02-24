d7ef3df5ebf702f499e45e060d8f53e3f8d23023
supersimetria <pfiliberto599@gmail.com>
2022-02-24 16:42:31 -0500

    001.004

--- 01.packages.sh
+++ 01.packages.sh
@@ -8,32 +8,34 @@
 printf "# ------------------------------------------------------------------------------\n"
 printf "# Download packeges\n"
 printf "# ------------------------------------------------------------------------------\n"
-sudo pacman -Sw $(curl https://raw.githubusercontent.com/supersimetria/L_manjaro/main/mod/pl-1) --noconfirm
+sudo pacman -Sw $(curl https://raw.githubusercontent.com/supersimetria/L_manjaro/main/mod/pl-2) --noconfirm
 printf "# ------------------------------------------------------------------------------\n"
 printf "# Install packeges\n"
 printf "# ------------------------------------------------------------------------------\n"
-sudo pacman -S $(curl https://raw.githubusercontent.com/supersimetria/L_manjaro/main/mod/pl-1) --noconfirm
+sudo pacman -S $(curl https://raw.githubusercontent.com/supersimetria/L_manjaro/main/mod/pl-2) --noconfirm
 printf "# ------------------------------------------------------------------------------\n"
 printf "# AUR packeges\n"
 printf "# ------------------------------------------------------------------------------\n"
 sudo sed -i '48s/.*/MAKEFLAGS="-j$(nproc)/' /etc/makepkg.conf
-yay -S anydesk-bin \
-  archtorify-git \
-  czkawka-gui-bin \
-  drawio-desktop-bin \
-  figma-linux \
-  flat-remix \
-  flat-remix-gtk \
-  freefilesync-bin \
-  iwscanner \
-  megasync \
-  nerd-fonts-fira-code \
-  obsidian \
-  quickhash-gui-bin \
-  spotify \
-  trilium-bin \
-  virtualbox-ext-oracle \
+yay -S spotify \
 --noconfirm
+
+  #anydesk-bin \
+  #archtorify-git \
+  #czkawka-gui-bin \
+  #drawio-desktop-bin \
+  #figma-linux \
+  #flat-remix \
+  #flat-remix-gtk \
+  #freefilesync-bin \
+  #iwscanner \
+  #megasync \
+  #nerd-fonts-fira-code \
+  #obsidian \
+  #quickhash-gui-bin \
+  #spotify \
+  #trilium-bin \
+  #virtualbox-ext-oracle \
 
 # dupeguru
 # joplin-desktop
